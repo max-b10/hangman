642448cb6f824a7887c6cf49fba718da11bc1ccf
max-b10 <bungaymax@gmail.com>
2021-02-15 11:27:51 +0000

    Updates

--- hangman/app.js
+++ hangman/app.js
@@ -6,5 +6,22 @@
 // String: myString --> String.prototype --> Object.prototype --> null
 // Number: myNumber --> Number.prototype --> Object.prototype --> null
 // Boolean: myBoolean: --> Boolean:.prototype --> Object.prototype --> null
-const product = "computer";
-console.log(product.split(""));
+// const product = "computer";
+// console.log(product.split(""));
+
+const game1 = new Hangman("cat", 5); // The word and guesses allowed
+const puzzleEl = document.querySelector("#puzzle");
+const guessesEl = document.querySelector("#guesses");
+
+// console.log(game1.getPuzzle());
+// console.log(game1.remainingGuesses);
+
+puzzleEl.textContent = game1.getPuzzle();
+guessesEl.textContent = game1.getStatusMessage();
+
+window.addEventListener("keypress", function (e) {
+  const guess = e.key;
+  game1.makeGuess(guess);
+  puzzleEl.textContent = game1.getPuzzle();
+  guessesEl.textContent = game1.getStatusMessage();
+});
--- hangman/hangman.js
+++ hangman/hangman.js
@@ -1,7 +1,35 @@
 const Hangman = function (word, remainingGuesses) {
   this.word = word.toLowerCase().split("");
   this.remainingGuesses = remainingGuesses;
-  this.guessedLetters = ["t"];
+  this.guessedLetters = [];
+  this.status = "playing";
+};
+
+// Logic to determine this.status:
+Hangman.prototype.calculateStatus = function () {
+  let finished = true;
+  this.word.forEach((letter) => {
+    if (this.guessedLetters.includes(letter)) {
+    } else {
+      finished = false;
+    }
+  });
+
+  if (this.remainingGuesses === 0) {
+    this.status = "You failed. Game Over!";
+  } else if (finished) {
+    this.status = "You Win!";
+  }
+};
+
+Hangman.prototype.getStatusMessage = function () {
+  if (this.status === "playing") {
+    return `Guesses left: ${this.remainingGuesses}`;
+  } else if (this.status === "You failed. Game Over!") {
+    return `Nice try. The word was "${this.word.join("")}".`;
+  } else {
+    return "You guessed the word correctly!";
+  }
 };
 
 Hangman.prototype.getPuzzle = function () {
@@ -17,5 +45,20 @@ Hangman.prototype.getPuzzle = function () {
   return puzzle;
 };
 
-const game = new Hangman("cat", 5);
-console.log(game.getPuzzle());
+Hangman.prototype.makeGuess = function (guess) {
+  guess = guess.toLowerCase();
+  const isUnique = !this.guessedLetters.includes(guess);
+  const isBadGuess = !this.word.includes(guess);
+
+  if (this.status !== "playing") {
+    return;
+  }
+
+  if (isUnique) {
+    this.guessedLetters.push(guess);
+  }
+  if (isUnique && isBadGuess) {
+    this.remainingGuesses--;
+  }
+  this.calculateStatus();
+};
