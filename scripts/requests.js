80aa32d0166e7855b4c8202c19e0b01a94f39b62
max-b10 <bungaymax@gmail.com>
2021-02-19 11:15:58 +0000

    Tweaked so it can be deployed to netifly

--- scripts/requests.js
+++ scripts/requests.js
@@ -1,7 +1,7 @@
 // Using Fetch:
 const getPuzzle = async (wordCount) => {
   const response = await fetch(
-    `http://puzzle.mead.io/puzzle?wordCount=${wordCount}`
+    `//puzzle.mead.io/puzzle?wordCount=${wordCount}`
   );
   if (response.status === 200) {
     const data = await response.json();
@@ -18,7 +18,7 @@ const getPuzzle = async (wordCount) => {
 
 // Using Fetch with async-await:
 const getCountry = async (countryCode) => {
-  const response = await fetch("http://restcountries.eu/rest/v2/all");
+  const response = await fetch("//restcountries.eu/rest/v2/all");
   if (response.status === 200) {
     const data = await response.json();
     return data.find((country) => country.alpha2Code === countryCode);
@@ -29,7 +29,7 @@ const getCountry = async (countryCode) => {
 
 // Using Fetch with async-await:
 const getLocation = async () => {
-  const response = await fetch("http://ipinfo.io/json?token=e437d942b50803");
+  const response = await fetch("//ipinfo.io/json?token=e437d942b50803");
   if (response.status === 200) {
     return response.json();
   } else {
